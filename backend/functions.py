ab2f000b491eaa9c91a5dbb2cd20fa007045ce88
Shuvam Shah <shvmks@outlook.com>
2020-08-02 17:34:09 +0530

    backend cleaned

--- backend/functions.py
+++ backend/functions.py
@@ -1,20 +1,23 @@
 from typing import Dict, List
 from data_models import Product, Color
+from settings import STATIC
 
-def get_top_n_products(cluster1: int, cluster2: int, n: int, fashion_data: Dict, **kwargs) -> List[Product]:
+def get_top_n_products(cluster1: int, cluster2: int, n: int, fashion_data: Dict) -> List[Product]:
   assert n >= 0, "n must be positive"
   
   results = []
-  base_path = kwargs.get("base_path", "")
   for k, v in fashion_data.items():
     if v['cluster_C1'] is None or v['cluster_C2'] is None:
       continue
     
     if int(v['cluster_C1']) == cluster1 and int(v['cluster_C2']) == cluster2:
-      results.append(Product(image=f"{base_path}{v['image_id']}",
+      results.append(Product(image=create_url(STATIC, v['image_id']),
                              pattern=v['pttrn_type'],
                              avg_rating=v['avg_rating'],
                              n_reviews=v['no_of_reviews'],
                              color3=Color(r=v['Color_3'][0], g=v['Color_3'][1], b=v['Color_3'][2]),
                              trend=v['Trend']))
   return sorted(results, key=lambda p: p.trend, reverse=True)[:n]
+
+def create_url(*args):
+  return '/'.join(s.strip('/') for s in args)
